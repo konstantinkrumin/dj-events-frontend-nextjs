edf790debcf4e0032cfe4e905753c4970d0fc133
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-27 19:04:17 +0300

    learning about routing in nextjs

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events.</p>
       <p>Version 1.0.0</p>
+      <Link href="/">Home</Link>
     </div>
   );
 };
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/Router';
+
 const EventPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Home Page</button>
     </div>
   );
 };
