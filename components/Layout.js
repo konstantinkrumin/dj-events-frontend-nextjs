35049a592e8fa99876152b95d57194723c3fa76a
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-27 20:33:13 +0300

    set up Showcase hero component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 import styles from '@/styles/Layout.module.css';
 
 const Layout = ({ title, keywords, description, children }) => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -13,6 +17,7 @@ const Layout = ({ title, keywords, description, children }) => {
       </Head>
 
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
