dc97a0ab1f657bc42c3324bbe4ffe672e1cfff76
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-02 18:22:26 +0300

    fixed the bug

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -8,6 +8,8 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  useEffect(() => checkUserLoggedIn(), []);
+
   // Register user
   const register = async (user) => {
     console.log(user);
@@ -40,7 +42,14 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log('Check');
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return <AuthContext.Provider value={{ user, error, register, login, logout }}>{children}</AuthContext.Provider>;
